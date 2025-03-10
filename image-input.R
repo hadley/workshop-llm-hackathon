beb1f88fdc69727e940d8f4ebf8b40b81047e50e
Hadley Wickham <h.wickham@gmail.com>
2025-03-10 17:13:37 +1100

    Add image input example

---
+++ image-input.R
@@ -0,0 +1,9 @@
+library(ellmer)
+
+chat <- chat_claude()
+
+chat$chat(
+  content_image_file("holly-mandarich-3p9zaNwUtv8-unsplash.jpg"),
+  "Describe this photo"
+)
+chat$chat("Where in the world do you think it is?")
